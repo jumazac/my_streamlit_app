3a4eb55b7ae55247cc7daf7f51ef77e9af6a1427
jumazac <133818773+jumazac@users.noreply.github.com>
2023-05-21 14:17:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,11 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-# Load your data
 df = pd.read_csv("TOTAL1.csv")
 
-
+# Generate counts for 'IN EVENT?'
 df_tree = df['IN EVENT?'].value_counts().reset_index()
 df_tree.columns = ['id', 'value']
-
 
 # Calculate total for percentage calculation
 total = df_tree['value'].sum()
@@ -18,13 +16,18 @@ total = df_tree['value'].sum()
 df_tree['percentage'] = df_tree['value'] / total * 100
 
 # Create the sunburst chart
-fig = px.sunburst(df_tree, path=['id'], values='value',
-                  hover_data={'id': True,
-                              'value': True,
-                              'percentage': ':.2f'},
-                  hovertemplate='<b>%{label}</b><br>Count: %{value}<br>Percentage: %{customdata[2]}%')
+fig = go.Figure(go.Sunburst(
+    labels=df_tree['id'],
+    parents=['' for _ in df_tree['id']],
+    values=df_tree['value'],
+    hovertext=['{:.2f}%'.format(p) for p in df_tree['percentage']],
+    hoverinfo="label+text+value"
+))
 
-# Show the chart
+# Update layout for the figure
+fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
+
+# Display the chart in Streamlit
 st.plotly_chart(fig)
 
 # Load your data
