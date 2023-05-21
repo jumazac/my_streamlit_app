76331c86c5f0a63c213838f03e0d4c06e41589a5
jumazac <133818773+jumazac@users.noreply.github.com>
2023-05-20 21:28:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,32 @@ import plotly.graph_objects as go
 # Load your data
 df = pd.read_csv("TOTAL1.csv")
 
-# Specify the levels, color column, and value column
-levels = ['IN EVENT?']
+# Specify the levels
+levels = ['LOCATION', 'IN EVENT?']
 
-df_all_trees = pd.DataFrame(columns=['id', 'parent', 'value'])
-df_all_trees['id'] = df[levels[0]]
-df_all_trees['parent'] = ""
-df_all_trees['value'] = 1
+def build_hierarchical_dataframe(df, levels):
+    """
+    Build a hierarchy of levels for Sunburst or Treemap charts.
+    """
+    df_all_trees = pd.DataFrame(columns=['id', 'parent', 'value'])
+    dfg = df.groupby(levels).size().reset_index(name='value')  # Use size here instead of sum
+    for i, level in enumerate(levels):
+        df_tree = pd.DataFrame(columns=['id', 'parent', 'value'])
+        df_tree['id'] = dfg[level].copy()
+        if i < len(levels) - 1:
+            df_tree['parent'] = dfg[levels[i+1]].copy()
+        else:
+            df_tree['parent'] = 'total'
+        df_tree['value'] = dfg['value']
+
+        df_all_trees = pd.concat([df_all_trees, df_tree], ignore_index=True)
+        
+    total = pd.Series(dict(id='total', parent='', 
+                           value=df.shape[0]))   # Use shape[0] here to get the total count
+    df_all_trees = pd.concat([df_all_trees, pd.DataFrame(total).T], ignore_index=True)
+    return df_all_trees
+
+df_all_trees = build_hierarchical_dataframe(df, levels)
 
 fig = go.Figure()
 
@@ -28,7 +47,6 @@ fig.add_trace(go.Sunburst(
 fig.update_layout(margin=dict(t=10, b=10, r=10, l=10))
 
 st.plotly_chart(fig)
-
 
 
 
