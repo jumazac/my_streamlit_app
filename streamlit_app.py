85d3894840c11ef07e55889ca3697e8854bc19b7
jumazac <133818773+jumazac@users.noreply.github.com>
2023-05-20 22:10:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,10 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 
+
 # Load your data
 df = pd.read_csv("TOTAL1.csv")
+print(df)
 
 # Ensure AGE is numeric (int or float)
 df['AGE'] = pd.to_numeric(df['AGE'], errors='coerce')
@@ -15,7 +17,7 @@ age_dict = {18: 0, 19: 1/6, 20: 2/6, 21: 3/6, 22: 4/6, 23: 5/6, 24: 1}
 df['color_value'] = df['AGE'].map(lambda age: age_dict[age])
 
 # Specify the levels, color column, and value column
-levels = ['IN EVENT?', 'LOCATION', 'Q1','Why 1','Q2','Why 2','USE SPIN ?','THINK SPIN','LIVE CAMPUS?','Where','AGE','SEX','color_value']
+levels = ['YEAR', 'SEX', 'AGE', 'Where', 'LIVE CAMPUS?', 'THINK SPIN', 'USE SPIN ?', 'Why 2', 'Q2', 'Why 1', 'Q1', 'LOCATION', 'IN EVENT?']
 color_column = 'color_value'
 value_column = 'YEAR'
 
@@ -38,7 +40,6 @@ def build_hierarchical_dataframe(df, levels, color_column=None):
         df_tree['value'] = dfg['value']
         if color_column:
             df_tree['color'] = dfg[color_column]
-
         df_all_trees = pd.concat([df_all_trees, df_tree], ignore_index=True)
         
     total = pd.Series(dict(id='total', parent='', 
@@ -48,10 +49,7 @@ def build_hierarchical_dataframe(df, levels, color_column=None):
     return df_all_trees
 
 df_all_trees = build_hierarchical_dataframe(df, levels, color_column)
-print(df_all_trees.head())
-print(df_all_trees.tail())
-
-st.write(df_all_trees)
+print(df_all_trees)
 
 fig = go.Figure()
 
@@ -71,6 +69,8 @@ fig.add_trace(go.Sunburst(
 fig.update_layout(margin=dict(t=10, b=10, r=10, l=10))
 
 st.plotly_chart(fig)
+
+print(df.columns)
 
 import streamlit as st
 
