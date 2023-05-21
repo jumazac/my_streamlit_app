6b497812fcda1b88d6b952a823e892039d943120
jumazac <133818773+jumazac@users.noreply.github.com>
2023-05-21 13:42:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,21 +3,20 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-
-
-
+# Load your data
 df = pd.read_csv("TOTAL1.csv")
 
-# Specify the levels
-levels = ['SEX', 'AGE', 'YEAR']
 
-def build_hierarchical_dataframe(df, levels):
+levels = ['YEAR']
+
+def build_hierarchical_dataframe(df, levels, color_column=None):
     """
     Build a hierarchy of levels for Sunburst or Treemap charts.
+
     Levels are given starting from the bottom to the top of the hierarchy,
     ie the last level corresponds to the root.
     """
-    df_all_trees = pd.DataFrame(columns=['id', 'parent', 'value'])
+    df_all_trees = pd.DataFrame(columns=['id', 'parent', 'value', 'color'])
     for i, level in enumerate(levels):
         df_tree = pd.DataFrame(columns=['id', 'parent', 'value'])
         dfg = df.groupby(levels[i:]).size().reset_index(name='value')
@@ -28,14 +27,25 @@ def build_hierarchical_dataframe(df, levels):
             df_tree['parent'] = 'total'
         df_tree['value'] = dfg['value']
         df_all_trees = pd.concat([df_all_trees, df_tree], ignore_index=True)
-        
+
     total = pd.Series(dict(id='total', parent='', value=df.shape[0]))
     df_all_trees = pd.concat([df_all_trees, pd.DataFrame(total).T], ignore_index=True)
     return df_all_trees
 
 df_all_trees = build_hierarchical_dataframe(df, levels)
 
-fig = px.sunburst(df_all_trees, path=['parent', 'id'], values='value')
+fig = go.Figure()
+
+fig.add_trace(go.Sunburst(
+    labels=df_all_trees['id'],
+    parents=df_all_trees['parent'],
+    values=df_all_trees['value'],
+    branchvalues='total',
+    hovertemplate='<b>%{label} </b> <br> Value: %{value}<br> Percentage of Total: %{percent:.2%}',
+    maxdepth=2
+))
+
+fig.update_layout(margin=dict(t=10, b=10, r=10, l=10))
 
 st.plotly_chart(fig)
 
