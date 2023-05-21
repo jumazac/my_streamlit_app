98e51e6b0ebd1f428b342c22836e47346557ff11
jumazac <133818773+jumazac@users.noreply.github.com>
2023-05-21 13:53:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,47 +7,20 @@ import plotly.graph_objects as go
 df = pd.read_csv("TOTAL1.csv")
 
 
-levels = ['IN EVENT?']
+df_tree = df['IN EVENT?'].value_counts().reset_index()
+df_tree.columns = ['id', 'value']
 
-# Function to build hierarchical DataFrame
-def build_hierarchical_dataframe(df, levels, value_column='count'):
-    d = dict(zip([f"level_{i+1}" for i in range(len(levels))], levels))
-    df_all_trees = pd.DataFrame(columns=['id', 'parent', 'value'])
-    
-    for i, level in enumerate(levels):
-        df_tree = pd.DataFrame(columns=['id', 'parent', 'value'])
-        d_temp = d.copy()
-        d_temp.pop(f"level_{i+1}")
-        df_grouped = df.groupby(by=list(d_temp.values())).count().reset_index()
-        df_tree['id'] = df_grouped[level].copy()
-        
-        if i < len(levels) - 1:
-            df_tree['parent'] = df_grouped[levels[i+1]].copy()
-        else:
-            df_tree['parent'] = 'total'
-        
-        df_tree['value'] = df_grouped[value_column]
-        df_all_trees = df_all_trees.append(df_tree, ignore_index=True)
-        
-    df_all_trees = df_all_trees.sort_values(by='id')
-    df_all_trees = df_all_trees.reset_index(drop=True)
-    total = pd.Series(dict(id='total', parent='',
-                           value=df[value_column].sum()))
-    df_all_trees = df_all_trees.append(total, ignore_index=True)
-    
-    return df_all_trees
-
-# Build the hierarchical DataFrame
-df_all_trees = build_hierarchical_dataframe(df, levels)
+# Add a 'total' row to the DataFrame
+total = df_tree['value'].sum()
+df_tree = df_tree.append({'id': 'total', 'value': total}, ignore_index=True)
 
 # Create the sunburst chart
-fig = px.sunburst(df_all_trees, 
-                  path=['parent', 'id'], 
+fig = px.sunburst(df_tree, 
+                  path=['id'], 
                   values='value')
 
 # Show the chart
 fig.show()
-
 
 # Load your data
 df = pd.read_csv("TOTAL1.csv")
