730373aeeaa60bb42f296ef863de6c9aebc8af12
jumazac <133818773+jumazac@users.noreply.github.com>
2023-05-22 21:24:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,54 +6,33 @@ import plotly.graph_objects as go
 # Load data
 df = pd.read_csv("TOTAL1.csv")
 
-# Count the occurrences for each combination of "IN EVENT?", "LOCATION", and "Q1".
-df_counts = df.groupby(['IN EVENT?', 'LOCATION', 'Q1']).size().reset_index(name='counts')
-
-levels = ['Q1', 'LOCATION', 'IN EVENT?']  # levels used for the hierarchical chart
-value_column = 'counts'
 
 def build_hierarchical_dataframe(df, levels, value_column, color_column=None):
     """
     Build a hierarchy of levels for Sunburst or Treemap charts.
-
-    Levels are given starting from the bottom to the top of the hierarchy,
-    ie the last level corresponds to the root.
     """
-    df_all_trees = pd.DataFrame(columns=['id', 'parent', 'value', 'color'])
+    df_all_trees = pd.DataFrame(columns=['id', 'parent', 'value'])
+
     for i, level in enumerate(levels):
-        df_tree = pd.DataFrame(columns=['id', 'parent', 'value'])
-        dfg = df.groupby(levels[i:]).sum(numeric_only=True)
-        dfg = dfg.reset_index()
-        df_tree['id'] = dfg[level].copy()
-        if i < len(levels) - 1:
-            df_tree['parent'] = dfg[levels[i+1]].copy()
+        df_tree = df.groupby(levels[:i + 1]).sum().reset_index()
+        if i < len(levels) - 1: 
+            # Not the last level
+            parents = levels[:i + 1]
+            df_tree.columns = parents + ['value']
         else:
-            df_tree['parent'] = 'total'
-        df_tree['value'] = dfg[value_column]
-        df_all_trees = pd.concat([df_all_trees, df_tree])
-        print("This is df_tree after concat with level {}: \n{}".format(level, df_tree))  # print df_tree here
-    total = pd.Series(dict(id='total', parent='',
-                           value=df[value_column].sum(),
-                           color='white' if color_column is None else df[color_column].sum()))
-    df_all_trees = df_all_trees.append(total, ignore_index=True)
-    print("This is df_all_trees after final append: \n{}".format(df_all_trees))  # print df_all_trees here
+            # This is the last level, so add 'id' and 'parent' columns
+            parents = levels[:i] if i > 0 else ['']
+            df_tree.columns = ['id'] + parents + ['value']
+
+        df_all_trees = pd.concat([df_all_trees, df_tree], axis=0, ignore_index=True)
+
+    total = pd.DataFrame(dict(id='total', parent='',
+                              value=df[value_column].sum(),
+                              color='white' if color_column is None else df[color_column].sum()), 
+                              index=[0])
+    df_all_trees = pd.concat([df_all_trees, total], axis=0, ignore_index=True)
+
     return df_all_trees
-
-df_all_trees = build_hierarchical_dataframe(df_counts, levels, value_column)
-
-# Create the sunburst chart
-fig = go.Figure(go.Sunburst(
-    labels=df_all_trees['id'],
-    parents=df_all_trees['parent'],
-    values=df_all_trees['value'],
-    branchvalues='total',
-    marker=dict(
-        colors=df_all_trees['value'],
-        colorscale='RdBu'),
-    hovertemplate='<b>%{label} </b> <br> Count: %{value}',
-))
-
-fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
 
 # Load your data
 df = pd.read_csv("TOTAL1.csv")
