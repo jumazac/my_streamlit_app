b8ca680004e0a1286ca5f4877ccc88e069275252
jumazac <133818773+jumazac@users.noreply.github.com>
2023-05-25 17:04:32 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,7 @@ levels = ['LOCATION', 'Q2', 'Q1', 'LIVE CAMPUS?', 'USE SPIN?', 'SEX', 'YEAR'] # 
 color_column = 'YEAR' 
 df_hierarchical = build_hierarchical_dataframe(df, levels, color_column)
 
+st.dataframe(df)
 
 
 # Load your data
