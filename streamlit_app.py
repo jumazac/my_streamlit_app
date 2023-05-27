1bd2393c6183f6c854218ba6f36c68060580337b
jumazac <133818773+jumazac@users.noreply.github.com>
2023-05-26 22:41:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,8 +50,7 @@ value_column = 'YEAR'
 color_column = ['YEAR'] 
 
 df_hierarchical = build_hierarchical_dataframe(df, levels, value_column)
-df_hierarchical['percentage'] = df_hierarchical.groupby('parent')['value'].apply(lambda x: x / x.sum() * 100)
-print(df_hierarchical.to_string())
+df_hierarchical['percentage'] = df_hierarchical.groupby(['parent'])['value'].apply(lambda x: x / x.sum() * 100)
 # Create the sunburst chart
 fig = go.Figure()
 
