04ca0e2ecc015e52a45d3cbc25315d56753c7833
jumazac <133818773+jumazac@users.noreply.github.com>
2023-05-28 17:04:04 -0600

    Update Q1sunburst.py

--- Q1sunburst.py
+++ Q1sunburst.py
@@ -11,24 +11,28 @@ df = df.fillna("N/A")
 
 
 def create_sunburst(df):
-    # Group the dataframe by 'Q1' and count the number of each unique value
-    grouped_df = df.groupby('Q1').size().reset_index(name='counts')
+    # Group the dataframe by 'Q1' and 'Why_1', and count the number of each unique pair
+    grouped_df = df.groupby(['Q1', 'Why_1']).size().reset_index(name='counts')
+
+    # Create a unique id for each 'Q1'-'Why_1' pair in the grouped DataFrame
+    grouped_df['id'] = grouped_df['Q1'].astype(str) + "-" + grouped_df['Why_1'].astype(str)
 
     # Compute total count to calculate the global percentage
     total_counts = grouped_df['counts'].sum()
 
-     # Compute global_percentage and percentage for each group
+    # Compute global_percentage and percentage for each group
     grouped_df['global_percentage'] = (grouped_df['counts'] / total_counts) * 100
     grouped_df['percentage'] = (grouped_df['counts'] / grouped_df['counts'].sum()) * 100
 
     fig = go.Figure(go.Sunburst(
-        labels=grouped_df['Q1'],  # Labels are unique 'Q1' values
-        parents=[""] * len(grouped_df),  # Parents are empty, as 'Q1' is the top level
-        values=grouped_df['counts'],  # The size of each slice is determined by the count of each unique value in 'Q1'
+        ids=grouped_df['id'],
+        labels=grouped_df['Why_1'],
+        parents=grouped_df['Q1'],
+        values=grouped_df['counts'],
         hovertemplate='<b>%{label} </b> <br> Count: %{value}<br> percentage: %{customdata[0]:.2f}<br> global_percentage: %{customdata[1]:.2f}',
-        customdata=list(zip(grouped_df.percentage, grouped_df.global_percentage)),  # Include both 'percentage' and 'global_percentage'
+        customdata=list(zip(grouped_df.percentage, grouped_df.global_percentage)),
     ))
     fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
-    return fig
 
+    return fig
 sunburst_chart = create_sunburst(df)
