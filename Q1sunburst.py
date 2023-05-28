2209b1f2f775f56f5a53b5542c77dc55e11000ed
jumazac <133818773+jumazac@users.noreply.github.com>
2023-05-28 12:30:40 -0600

    Update Q1sunburst.py

--- Q1sunburst.py
+++ Q1sunburst.py
@@ -2,20 +2,19 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 
-# Load data 
 df = pd.read_csv("TOTAL1NOTEPAD.txt", delimiter=',')
 
 # Fill NaN values
 df = df.fillna("N/A")
 
-# Print the first 5 rows of the DataFrame
-print(df[['Q1', 'Why_1', 'Q2', 'Why_2']].head())
+# Create a unique id for each row
+df['id'] = df['Q1'] + "-" + df['Why_1'] + "-" + df['Q2'] + "-" + df['Why_2']
 
 def create_sunburst(df):
     fig = go.Figure(go.Sunburst(
         labels=df['Why_2'], 
         parents=df['Why_1'],
-        ids=df['Q2'],
+        ids=df['id'],
         hovertext=df['Why_2'],
     ))
     fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
@@ -23,10 +22,4 @@ def create_sunburst(df):
 
 sunburst_chart = create_sunburst(df)
 
-# Print some properties of the sunburst chart
-print("Labels in the sunburst chart: ", sunburst_chart['data'][0]['labels'][:10])
-print("Parents in the sunburst chart: ", sunburst_chart['data'][0]['parents'][:10])
-print("IDs in the sunburst chart: ", sunburst_chart['data'][0]['ids'][:10])
-
-# Display the new sunburst chart in Streamlit
 st.plotly_chart(sunburst_chart)
