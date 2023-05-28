2a271ec2cb1534fa05d582dc0f7b0f3eb14232f7
jumazac <133818773+jumazac@users.noreply.github.com>
2023-05-28 16:50:15 -0600

    Update Q1sunburst.py

--- Q1sunburst.py
+++ Q1sunburst.py
@@ -14,10 +14,23 @@ def create_sunburst(df):
     # Group the dataframe by 'Q1' and count the number of each unique value
     grouped_df = df.groupby('Q1').size().reset_index(name='counts')
 
+    # Compute total count to calculate the global percentage
+    total_counts = grouped_df['counts'].sum()
+
+     # Compute global_percentage and percentage for each group
+    grouped_df['global_percentage'] = (grouped_df['counts'] / total_counts) * 100
+    grouped_df['percentage'] = (grouped_df['counts'] / grouped_df['counts'].sum()) * 100
+
+
+
+
+
     fig = go.Figure(go.Sunburst(
         labels=grouped_df['Q1'],  # Labels are unique 'Q1' values
         parents=[""] * len(grouped_df),  # Parents are empty, as 'Q1' is the top level
         values=grouped_df['counts'],  # The size of each slice is determined by the count of each unique value in 'Q1'
+        hovertemplate='<b>%{label} </b> <br> Count: %{value}<br> percentage: %{customdata[0]:.2f}<br> global_percentage: %{customdata[1]:.2f}',
+        customdata=list(zip(grouped_df.percentage, grouped_df.global_percentage)),  # Include both 'percentage' and 'global_percentage'
     ))
     fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
     return fig
