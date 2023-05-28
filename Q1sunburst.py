0748d2017db9ba8b8f7a826ab0fb782d20099967
jumazac <133818773+jumazac@users.noreply.github.com>
2023-05-28 16:37:02 -0600

    Update Q1sunburst.py

--- Q1sunburst.py
+++ Q1sunburst.py
@@ -11,20 +11,13 @@ df = df.fillna("N/A")
 
 
 def create_sunburst(df):
-    # Create a unique id for each row
-    df['id'] = df.index.astype(str) + "-" + df['Q1'].astype(str) + "-" + df['Why_1'].astype(str) + "-" + df['Q2'].astype(str) + "-" + df['Why_2'].astype(str)
-    
-    print(df)
+    df['id'] = df.index.astype(str) + "-" + df['Q1'].astype(str)
 
-
-# Define the label and parent for each row based on the hierarchy 'Q1', 'Why_1', 'Q2', 'Why_2'
-    df['label'] = df['Q1'] + " " + df['Why_1'] + " " + df['Q2'] + " " + df['Why_2']
-    df['parent'] = df['Q1'] + " " + df['Why_1'] + " " + df['Q2']
     fig = go.Figure(go.Sunburst(
-        labels=df['Why_2'],  # This should be a column with the names for each of the individual sections of the chart
-        parents=df['Why_1'],  # This should be a column with the names for the parent category of each section
-        ids=df['id'],  # This should be a column with unique identifiers for each section
-        hovertext=df['Why_2'],
+        labels=df['Q1'],  # Labels are Q1 values
+        parents=[""] * len(df),  # Parents are empty, as Q1 is the top level
+        ids=df['id'],  # ids are the unique identifiers we created
+        hovertext=df['Q1'],
     ))
     fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
     return fig
