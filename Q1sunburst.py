5b54a7e03352f543ec7d986c5eacf63178dcba96
jumazac <133818773+jumazac@users.noreply.github.com>
2023-05-29 17:25:20 -0600

    aaa

--- Q1sunburst.py
+++ Q1sunburst.py
@@ -3,16 +3,14 @@ import plotly.graph_objects as go
 import streamlit as st
 
 
-import pandas as pd
-import plotly.graph_objects as go
 
 def create_sunburst(df):
     # Preprocessing steps
     df = df.copy()
     df.fillna("N/A", inplace=True)
 
-    # Group the dataframe by 'Q1' and 'Why_1' to get counts
-    df_counts = df.groupby(['Q1', 'Why_1']).size().reset_index(name='counts')
+    # Group the dataframe by 'Q1', 'Why_1', and 'Q2' to get counts
+    df_counts = df.groupby(['Q1', 'Why_1', 'Q2']).size().reset_index(name='counts')
 
     # Create a DataFrame for the root 'Total'
     df_total = pd.DataFrame({"id": ["Total"], "parent": [""], "counts": [df.shape[0]]})
@@ -22,11 +20,20 @@ def create_sunburst(df):
     df_q1.columns = ['id', 'counts']
     df_q1['parent'] = 'Total'
 
-    df_why1 = df_counts.copy()
-    df_why1.columns = ['parent', 'id', 'counts']
+    df_why1 = df_counts[['Q1', 'Why_1', 'counts']].copy()
+    df_why1['id'] = df_why1['Q1'] + '-' + df_why1['Why_1']
+    df_why1['parent'] = df_why1['Q1']
+    df_why1 = df_why1[['parent', 'id', 'counts']]
+
+    # Prepare the 'Q2' level data:
+    df_q2 = df_counts.copy()
+    df_q2['id'] = df_counts['Q1'] + "-" + df_counts['Why_1'] + "-" + df_counts['Q2'] 
+    df_q2['parent'] = df_counts['Q1'] + "-" + df_counts['Why_1']
+    df_q2 = df_q2[['id', 'parent', 'counts']]   
 
     # Concatenate all DataFrames
-    df_sunburst = pd.concat([df_total, df_q1, df_why1])
+    df_sunburst = pd.concat([df_total, df_q1, df_why1, df_q2])
+    print(df_sunburst)  # print statement to check the DataFrame
 
     # Create sunburst chart
     fig = go.Figure(go.Sunburst(
@@ -39,4 +46,9 @@ def create_sunburst(df):
     fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
 
     return fig
+
+
+
+
+
 
