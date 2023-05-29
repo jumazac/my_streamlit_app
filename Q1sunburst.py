2d25c84a4be7f2545d5f1970f8d19bd0be785a2b
jumazac <133818773+jumazac@users.noreply.github.com>
2023-05-29 12:52:35 -0600

    Update Q1sunburst.py

--- Q1sunburst.py
+++ Q1sunburst.py
@@ -11,21 +11,16 @@ def create_sunburst(df):
     # Group the dataframe by 'Q1' and 'Why_1' to get counts
     df_counts = df.groupby(['Q1', 'Why_1']).size().reset_index(name='counts')
 
-    # Calculate global count
-    total_count = df.shape[0]
-    
     # Create a DataFrame for the root 'Total'
-    df_total = pd.DataFrame({"id": ["Total"], "parent": [""], "counts": [total_count], "percentage": [100]})
+    df_total = pd.DataFrame({"id": ["Total"], "parent": [""], "counts": [df.shape[0]]})
 
     # Create DataFrames for 'Q1' and 'Why_1' levels
     df_q1 = df_counts[['Q1', 'counts']].groupby('Q1').sum().reset_index()
     df_q1.columns = ['id', 'counts']
     df_q1['parent'] = 'Total'
-    df_q1['percentage'] = df_q1['counts'] / total_count * 100
 
     df_why1 = df_counts.copy()
     df_why1.columns = ['parent', 'id', 'counts']
-    df_why1['percentage'] = df_why1.groupby('parent')['counts'].apply(lambda x: x / x.sum() * 100)
 
     # Concatenate all DataFrames
     df_sunburst = pd.concat([df_total, df_q1, df_why1])
@@ -35,10 +30,8 @@ def create_sunburst(df):
         ids=df_sunburst['id'],
         labels=df_sunburst['id'], 
         parents=df_sunburst['parent'],
-        values=df_sunburst['counts'],
+        values=df_sunburst['counts'], 
         branchvalues='total',
-        hovertext=df_sunburst.apply(lambda row: f"{row['id']}<br>Percentage: {row['percentage']:.2f}%", axis=1),
-        hoverinfo='text'
     ))
     fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
 
