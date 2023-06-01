4a91aa2b1aa2030944cdd21581f5c0ea965f6299
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-01 16:28:06 -0600

    wqw

--- Q1sunburst.py
+++ Q1sunburst.py
@@ -158,8 +158,8 @@ def create_sunburst_chartSpin(df):
     print(df['THINK_SPIN'].unique())
 
     # Append column name to each value to ensure uniqueness
-    df['USE_SPIN?'] = 'USE_SPIN?: ' + df['USE_SPIN?'].str.upper().str.strip()
-    df['THINK_SPIN'] = 'THINK_SPIN: ' + df['THINK_SPIN'].str.upper().str.strip()
+    df['USE_SPIN?'] = 'USE_SPIN?:' + df['USE_SPIN?'].str.upper().str.strip()
+    df['THINK_SPIN'] = 'THINK_SPIN:' + df['THINK_SPIN'].str.upper().str.strip()
 
     # Compute counts for each combination of 'USE_SPIN?' and 'THINK_SPIN'
     df_sunburst = df.groupby(['USE_SPIN?', 'THINK_SPIN']).size().reset_index(name='counts')
