aeea21d152cbed58cc6d60b927ff22753440327e
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-04 16:02:28 -0600

    Update Q1sunburst.py

--- Q1sunburst.py
+++ Q1sunburst.py
@@ -198,8 +198,8 @@ def create_sunburst_chartSpin(df):
 
     fig.update_layout(
     title_text="Main chart",
-    width=500,  # Set the width of the chart
-    height=500  # Set the height of the chart
+    width=450,  # Set the width of the chart
+    height=450  # Set the height of the chart
 )
     
     return fig
