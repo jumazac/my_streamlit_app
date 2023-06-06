669ddf2057e4b5237bc7fbaee919226e737fb91a
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-05 19:49:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -125,9 +125,12 @@ fig.add_trace(go.Sunburst(
     marker=dict(
         colors=df_hierarchical['color']
     ),
-    hovertemplate='<b style="font-size: 18px;">%{label} </b> <br> <span style="font-size: 18px;">Count: %{value}<br> Path %{id}<br> percentage: %{customdata[0]:.2f}<br> global_percentage: %{customdata[1]:.2f}</span>',
+    hovertemplate='<b style="font-size: 16px;">%{label} </b> <br> <span style="font-size: 14px;">Count: %{value}<br> Path %{id}<br> percentage: %{customdata[0]:.2f}<br> global_percentage: %{customdata[1]:.2f}</span>',
     customdata=df_hierarchical['custom_data'],  # Here is where you include both 'percentage' and 'global_percentage'
-    maxdepth=3
+    maxdepth=2,
+    insidetextfont=dict(size=20),  # adjust size as needed
+    outsidetextfont=dict(size=20)  # adjust size as neede
+    
 
     
 ))
