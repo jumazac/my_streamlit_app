525a85407b404acb64cfaf4678cd7eb3d0681625
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-05 18:14:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -143,27 +143,6 @@ fig.update_layout(
     title_font=dict(size=30),  # Adjust the size as needed
 )
 
-
-cols = st.columns([1,2,1]) 
-cols[1].plotly_chart(fig)
-
-
-
-# Create the legend
-legend_cols = st.columns(5)
-
-
-legend_cols[0].markdown("<p style='text-align: center;'><div style='width: 20px; height: 20px; background: #0168c8;'></div> 1ST YEAR</p>", unsafe_allow_html=True)
-legend_cols[1].markdown("<p style='text-align: center;'><div style='width: 20px; height: 20px; background: #82c9fe;'></div> 2ND YEAR</p>", unsafe_allow_html=True)
-legend_cols[2].markdown("<p style='text-align: center;'><div style='width: 20px; height: 20px; background: #ffabab;'></div> 3RD YEAR</p>", unsafe_allow_html=True)
-legend_cols[3].markdown("<p style='text-align: center;'><div style='width: 20px; height: 20px; background: #ff2a2a;'></div> 4TH YEAR</p>", unsafe_allow_html=True)
-legend_cols[4].markdown("<p style='text-align: center;'><div style='width: 20px; height: 20px; background: #28b09c;'></div> MASTERS</p>", unsafe_allow_html=True)
-
-
-# Add space
-st.markdown("<br><br><br><br>", unsafe_allow_html=True)
-
-
 # Create the columns for the layout
 cols = st.columns([1,1]) 
 
@@ -190,6 +169,28 @@ df_labels = pd.DataFrame({
 
 # Display the DataFrame as a table in the second column
 cols[1].table(df_labels)
+
+
+
+cols = st.columns([1,2,1]) 
+cols[1].plotly_chart(fig)
+
+
+
+# Create the legend
+legend_cols = st.columns(5)
+
+
+legend_cols[0].markdown("<p style='text-align: center;'><div style='width: 20px; height: 20px; background: #0168c8;'></div> 1ST YEAR</p>", unsafe_allow_html=True)
+legend_cols[1].markdown("<p style='text-align: center;'><div style='width: 20px; height: 20px; background: #82c9fe;'></div> 2ND YEAR</p>", unsafe_allow_html=True)
+legend_cols[2].markdown("<p style='text-align: center;'><div style='width: 20px; height: 20px; background: #ffabab;'></div> 3RD YEAR</p>", unsafe_allow_html=True)
+legend_cols[3].markdown("<p style='text-align: center;'><div style='width: 20px; height: 20px; background: #ff2a2a;'></div> 4TH YEAR</p>", unsafe_allow_html=True)
+legend_cols[4].markdown("<p style='text-align: center;'><div style='width: 20px; height: 20px; background: #28b09c;'></div> MASTERS</p>", unsafe_allow_html=True)
+
+
+# Add space
+st.markdown("<br><br><br><br>", unsafe_allow_html=True)
+
 
 
 # Define three columns for the other charts
