c63ae9fd86cc0ab3002393d421912325305b8765
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-06 21:12:54 -0600

    Update Q1sunburst.py

--- Q1sunburst.py
+++ Q1sunburst.py
@@ -63,7 +63,7 @@ def create_sunburst_chartQs(df):
     parents=df_sunburst['parent'],
     values=df_sunburst['counts'], 
     customdata=df_sunburst[['local_percent', 'global_percent']],  # add local_percent and global_percent to customdata
-    hovertemplate='<b style="font-size: 18px;">%{label} </b> <br> <span style="font-size: 18px;">Count: %{value}<br> Path %{id}<br> Local: %{customdata[0]}%<br> Global: %{customdata[1]}%</span>',  # access customdata in hovertemplate
+    hovertemplate='<b style="font-size: 16px;">%{label} </b> <br> <span style="font-size: 14px;">Count: %{value}<br> Path %{id}<br> Local: %{customdata[0]:.1f}%<br> Global: %{customdata[1]:.1f}%</span>',  # access customdata in hovertemplate, limit to one decimal point, and adjust font size
     branchvalues='total',
     maxdepth=3,
 ))
