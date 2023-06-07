8423f8a66c28d497ae5a8fb11580c776fcd8ef3b
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-06 20:49:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -258,7 +258,7 @@ styled_table = f"""
     th, td {{
         text-align: left;
         padding: 8px;
-        font-size: 15px;
+        font-size: 10px;
     }}
     tr:nth-child(even) {{
         background-color: #f2f2f2;
