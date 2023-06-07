cdf33dc44f91c695b97f2e0e2bb888aa6bf8d705
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-06 21:23:39 -0600

    s

--- Q1sunburst.py
+++ Q1sunburst.py
@@ -150,20 +150,7 @@ def create_sunburst_chartCampus(df):
         maxdepth=3,
     ))
     fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
-    
-# Create and display the legend table
 
-    legend = """
-<div style='display: flex; justify-content: space-between;'>
-    <div>
-        <div style='width: 20px; height: 20px; background: #ff2a2a;'></div> 
-        <p>NO</p>
-    </div>
-    <div>
-        <div style='width: 20px; height: 20px; background: #0168c8;'></div> 
-        <p>YES</p>
-    </div>
-"""
 
 
     return fig
