cb59cf528321c72d8fab79811def9671ee9faf48
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-07 20:32:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,8 @@ st.markdown("""
     </div>
 """, unsafe_allow_html=True)
 
-col1.image('C:\\Users\JZ\Desktop\AI_CONVERTED\Logo.png')
+col2.subheader("Your Subtitle Here")
+
 
 # Add space
 st.markdown("""
