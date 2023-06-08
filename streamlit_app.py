dbd21a334328e478b3c0e56f9e70d35d69cf5a49
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-07 20:26:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ st.markdown("""
     </div>
 """, unsafe_allow_html=True)
 
-col1.image('C:\\Users\\JZ\\Desktop\\AI CONVERTED\\Logo.jpg')
+col1.image('C:\\Users\\JZ\\Desktop\\AI CONVERTED\\Logo.png')
 
 # Add space
 st.markdown("""
