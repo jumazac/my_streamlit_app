a718cbb2ae5683b67609808d2557b02fb08f5ed5
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-07 21:32:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,15 +43,13 @@ col4, col5, col6 = st.columns(3)
 col6.markdown("<p style='font-size:40px;'>What is this about?</p>", unsafe_allow_html=True)
 
 # Line break or space of 6 lines in the far right column
-col6.markdown("<br>"*6, unsafe_allow_html=True)
+col6.markdown("<br>"*12, unsafe_allow_html=True)
 
 # Write more text in the far right column of the new row
 col6.markdown("<p style='font-size:40px;'>The distinctive edge we provide?</p>", unsafe_allow_html=True)
 
 # Define a second new set of three columns
 col7, col8, col9 = st.columns(3)
-
-col6.markdown("<br>"*6, unsafe_allow_html=True)
 
 
 # Write some text in the first column of the second new row
