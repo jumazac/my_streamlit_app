787f602c01672cb3d7e1135df0a60ac03818566a
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-09 20:10:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,6 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 
-
-
 # Define three columns
 col1, col2, col3 = st.columns(3)
 
@@ -28,10 +26,10 @@ st.markdown("# Ubike GO ")
 # Create a custom header with links
 st.markdown("""
     <div style="display: flex; justify-content: space-around; background-color: #F63366; padding: 10px;">
-        <h2 style="color: white; margin: 0;"><a style="color: white; text-decoration: none;" href='https://example.com/link1'>HOME</a></h2>
-        <h2 style="color: white; margin: 0;"><a style="color: white; text-decoration: none;" href='https://example.com/link1'>Market analysis</a></h2>
-        <h2 style="color: white; margin: 0; margin-left: 80px;"><a style="color: white; text-decoration: none;" href='https://example.com/link2'>MAP</a></h2>
-        <h2 style="color: white; margin: 0;"><a style="color: white; text-decoration: none;" href='https://example.com/link3'>Why it is imperative</a></h2>
+        <h2 style="color: white; margin: 0;"><a style="color: white; text-decoration: none;" href='#home'>HOME</a></h2>
+        <h2 style="color: white; margin: 0;"><a style="color: white; text-decoration: none;" href='#market-analysis'>Market analysis</a></h2>
+        <h2 style="color: white; margin: 0; margin-left: 80px;"><a style="color: white; text-decoration: none;" href='#map'>MAP</a></h2>
+        <h2 style="color: white; margin: 0;"><a style="color: white; text-decoration: none;" href='#why-it-is-imperative'>Why it is imperative</a></h2>
     </div>
 """, unsafe_allow_html=True)
 
