091ea9959be3183e759c894e4345b3860d98bb73
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-13 14:50:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -461,6 +461,9 @@ with col2:
 
 st.markdown("<h2 id='why-it-is-imperative'>Why it is imperative</h2>", unsafe_allow_html=True)
 
-
+# Create the "why imperative" 
+col1, col2, col3 = st.columns((2, 1, 2))
+with col2:
+    st.image("https://i.imgur.com/AHwfyTu.png", caption='Maslow hierarchy of the U students', width=600)
 
 
