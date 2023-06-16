002c16a8d7cd922c45de3e60a86592a9061c5bc9
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-15 20:20:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -509,7 +509,7 @@ with col1[0]:
     view_state = pdk.ViewState(
         latitude=40.7648,  # Coordinates for the University of Utah
         longitude=-111.8421,
-        zoom=16,
+        zoom=15,
         pitch=0
     )
 
