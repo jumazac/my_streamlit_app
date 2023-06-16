6d86e2a6f9abf26ce5e3ff41f4d316508e03d571
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-16 16:13:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -476,11 +476,11 @@ with col4:
     # Define the data with number of line breaks
     data = {
         "Benefits": [
-            {"text": "Surrounding all Concepts (Quality of Life): Our e-bike service enhances students' quality of life by addressing cost, time, convenience, health, and academic impact.", "breaks": 1, "color": "rgba(255, 0, 0, 0.5)"},
-            {"text": "Self-Actualization (Academic Impact): By reducing commuting inefficiencies, our service allows students to focus more on their academic goals and personal growth.", "breaks": 2, "color": "rgba(0, 0, 255, 0.17)"},
-            {"text": "Esteem (Physical Health): E-bike riding promotes physical health and well-being, boosting self-esteem through improved fitness and increased energy levels.", "breaks": 3, "color": "rgba(0, 128, 0, 0.5)"},
-            {"text": "Belonging (Autonomy and Well-being): Our e-bike service offers an independent mode of transport, fostering a sense of belonging and connection with the university environment.", "breaks": 3, "color": "rgba(128, 0, 128, 0.5)"},
-            {"text": "Safety Needs (Time Efficiency):With readily available e-bikes, students can commute quickly across campus, saving time for academic pursuits and leisure activities.", "breaks": 3, "color": "rgba(255, 165, 0, 0.5)"},
+            {"text": "Surrounding all Concepts (Quality of Life): Our e-bike service enhances students' quality of life by addressing cost, time, convenience, health, and academic impact.", "breaks": 1, "color": "rgba(50,153,205,5)"},
+            {"text": "Self-Actualization (Academic Impact): By reducing commuting inefficiencies, our service allows students to focus more on their academic goals and personal growth.", "breaks": 2, "color": "rgba(147,38,143,255)"},
+            {"text": "Esteem (Physical Health): E-bike riding promotes physical health and well-being, boosting self-esteem through improved fitness and increased energy levels.", "breaks": 3, "color": "rgba(0,166,81,255)"},
+            {"text": "Belonging (Autonomy and Well-being): Our e-bike service offers an independent mode of transport, fostering a sense of belonging and connection with the university environment.", "breaks": 3, "color": "rgba(230,231,20,255)"},
+            {"text": "Safety Needs (Time Efficiency):With readily available e-bikes, students can commute quickly across campus, saving time for academic pursuits and leisure activities.", "breaks": 3, "color": "rgba(255,186,19,255)"},
             {"text": "Physiological Needs (Cost Efficiency): Our e-bike service saves students money by eliminating car ownership, gas, and parking costs, providing a budget-friendly transportation option", "breaks": 2, "color": "rgba(243,130,47,255)"},
         ]
     }
