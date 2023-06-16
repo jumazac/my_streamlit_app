38d0ad326c4c1b22412deae7f7b4066c82b26052
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-16 16:03:29 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -481,7 +481,7 @@ with col4:
             {"text": "Esteem (Physical Health): E-bike riding promotes physical health and well-being, boosting self-esteem through improved fitness and increased energy levels.", "breaks": 3, "color": "rgba(0, 128, 0, 0.5)"},
             {"text": "Belonging (Autonomy and Well-being): Our e-bike service offers an independent mode of transport, fostering a sense of belonging and connection with the university environment.", "breaks": 3, "color": "rgba(128, 0, 128, 0.5)"},
             {"text": "Safety Needs (Time Efficiency):With readily available e-bikes, students can commute quickly across campus, saving time for academic pursuits and leisure activities.", "breaks": 3, "color": "rgba(255, 165, 0, 0.5)"},
-            {"text": "Physiological Needs (Cost Efficiency): Our e-bike service saves students money by eliminating car ownership, gas, and parking costs, providing a budget-friendly transportation option", "breaks": 2, "color": "rgba(255, 105, 180, 2)"},
+            {"text": "Physiological Needs (Cost Efficiency): Our e-bike service saves students money by eliminating car ownership, gas, and parking costs, providing a budget-friendly transportation option", "breaks": 2, "color": "rgba(255, 105, 180, 0.2)"},
         ]
     }
     for benefit in data["Benefits"]:
