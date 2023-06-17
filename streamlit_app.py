c5c8d8deee840aa1381e9b43da7fd043f8661b55
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-16 20:38:25 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ body {
     """, unsafe_allow_html=True)
 
 # Define two columns
-col1, col2 = st.columns([2, 1])
+col1, col2 = st.columns([1, 2])
 
 # Place the logo in the first column
 col1.markdown("![LOGO](https://i.imgur.com/DBZxXXg.png)")
