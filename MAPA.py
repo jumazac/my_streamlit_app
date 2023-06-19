90bb7b345d8d8930020abb2654ae2a0eee92ef5b
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-18 19:51:08 -0600

    Update MAPA.py

--- MAPA.py
+++ MAPA.py
@@ -28,9 +28,9 @@ def generate_map():
     for feature in geojson_data['features']:
         # existing conditions
         if feature['properties']['Name'] == '1RA CONCETRACION, 8:45-19:45':
-            feature['properties']['color'] = [255, 0, 0]  # RGB color for red
+            feature['properties']['color'] = [255, 0, 0, 95]  # RGB color for red
         elif feature['properties']['Name'] == '3RA CONCENTRACION, 10:00-22:00':
-            feature['properties']['color'] = [255, 255, 0]  # RGB color for yellow
+            feature['properties']['color'] = [255, 255, 0, 95]  # RGB color for yellow
         elif feature['properties']['Name'] == '2DA CONCERNTRACION, 10:00-22:00':
             feature['properties']['color'] = [255, 165, 0, 95]
         elif feature['properties']['Name'] == 'Polygon 150':
