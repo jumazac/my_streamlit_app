a2974221a0bbd9ba0c3daa3e68aaf61d4f93a60f
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-18 18:26:01 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -746,8 +746,8 @@ r = generate_map()
 
 st.markdown("""
     <iframe 
-        width='300%' 
-        height='700px' 
+        width='100%' 
+        height='500px' 
         src="https://api.mapbox.com/styles/v1/jumazac/clj1wx5ue00rm01r7hr755dsv.html?title=false&access_token=pk.eyJ1IjoianVtYXphYyIsImEiOiJjbGowZHhxZXIwNTh4M2R0OHZzNW5oamhoIn0.22XzPjwDWm9etY_unBN4Qg&zoomwheel=false#15.9/40.764035/-111.841069" 
         title="Streets" 
         style="border:none;">
@@ -755,9 +755,6 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 
-st.pydeck_chart(r)
 
-# Generate the map
-r = generate_map()
 
 
