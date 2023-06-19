eb6a987cbb3165bf3da08cb10b9cf28dd10a7e6c
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-19 17:52:47 -0600

    Update MAPA.py

--- MAPA.py
+++ MAPA.py
@@ -34,9 +34,9 @@ def generate_map():
             feature['properties']['color'] = [44, 165, 0, 95]
         elif feature['properties']['Name'] == '3RA CONCENTRACION, 10:00-22:00':
             feature['properties']['color'] = [255, 255, 0, 95]  # RGB color for yellow
-        elif feature['properties']['Name'] == 'Polygon 150':
-            feature['properties']['color'] = [0, 0, 0,]
-       
+        
+        elif feature['properties']['Name'] in ['Polygon 135', 'Polygon 138', 'Polygon 139', 'Polygon 148', 'Polygon 147', 'Polygon 133', 'Polygon 140', 'UNION']:
+            feature['properties']['color'] = [0, 0, 0]  # RGB color for black
         elif feature['properties']['Name'] == 'RUTA 1, 1 MILLA':
             feature['properties']['color'] = [0, 84, 255]
         elif feature['properties']['Name'] in purple_points:
