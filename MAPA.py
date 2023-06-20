6dc183c087695d3fa5ad3fde481047d7f0f05464
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-19 18:43:54 -0600

    Update MAPA.py

--- MAPA.py
+++ MAPA.py
@@ -91,7 +91,7 @@ def generate_map():
         "TextLayer",
         pd.DataFrame({
             'text': ['Red: Label 1', 'Blue: Label 2', 'Green: Label 3'],
-            'position': [[-111.838860, 40.765313+0.0001*i] for i in range(3)],  # Slightly shift the legend position
+            'position': [[-111.838860, 40.765313+0.001*i] for i in range(3)],  # Slightly shift the legend position
         }),
         get_position='position',
         get_text='text',
