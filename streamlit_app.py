845aa7e1954ee00d7b8af1ddb774f4c60d9c5ec8
jumazac <133818773+jumazac@users.noreply.github.com>
2023-06-19 21:38:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,10 +101,10 @@ col4.markdown("""
 - Aligned with Government Objectives: In line with current federal and district commuting goals.
 """, unsafe_allow_html=True)
 
-col4.markdown("<br><br><br><br><br>", unsafe_allow_html=True)
+st.markdown("<br><br><br><br><br>", unsafe_allow_html=True)
 
 
-col4.markdown("<p style='font-size:17px;'>BUS<br>TRAX<br>WALKING<br>SPIN</p>", unsafe_allow_html=True)
+st.markdown("<p style='font-size:17px;'>BUS<br>TRAX<br>WALKING<br>SPIN</p>", unsafe_allow_html=True)
 
 
 
